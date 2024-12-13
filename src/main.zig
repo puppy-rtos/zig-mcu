0cbd40105a58eef9cc643f4d75c6ddbbfc082d4f
guozhanxin <guozhanxin@rt-thread.com>
2024-12-13 08:12:23 +0800

    update readme

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,10 @@
 const std = @import("std");
 
 pub fn add(a: u8, b: u8) u8 {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     return a + b;
 }
 
 export fn main() void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     const c = add(10, 11);
 
     _ = c;
@@ -14,6 +12,5 @@ export fn main() void {
 }
 
 export fn subcpu_entry() void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     while (true) {}
 }
